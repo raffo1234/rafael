84bde32e72dee6df0d4b9366e0d4a0b92c025214
Rafael <rhmcord@gmail.com>
2025-03-24 08:07:45 -0500

    feat: remove unused import

---
+++ .vercel/output/_functions/chunks/Container_DaKfQhHo.mjs
@@ -0,0 +1,9 @@
+import { c as createComponent, r as renderTemplate, m as maybeRenderHead, a as renderSlot } from './astro/server_fnSuBzlO.mjs';
+import 'kleur/colors';
+import 'clsx';
+
+const $$Container = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<main class="p-2 md:p-6 lg:p-9 flex w-full"> ${renderSlot($$result, $$slots["default"])} </main>`;
+}, "C:/github/rafael/src/components/Container.astro", void 0);
+
+export { $$Container as $ };
