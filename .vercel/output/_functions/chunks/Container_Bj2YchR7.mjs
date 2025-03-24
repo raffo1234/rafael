71bbbc0fa50cfdbdcc783118e6a0bf4831c89a57
Rafael <rhmcord@gmail.com>
2025-03-24 08:18:38 -0500

    fix: remove .vercel folder

--- .vercel/output/_functions/chunks/Container_Bj2YchR7.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createComponent, r as renderTemplate, m as maybeRenderHead, a as renderSlot } from './astro/server_ComAydu-.mjs';
-import 'kleur/colors';
-import 'clsx';
-
-const $$Container = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${maybeRenderHead()}<main class="p-2 md:p-6 lg:p-9 flex w-full"> ${renderSlot($$result, $$slots["default"])} </main>`;
-}, "C:/github/rafael/src/components/Container.astro", void 0);
-
-export { $$Container as $ };
--- .vercel/output/_functions/chunks/supabase_BHY3KBkp.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://fdphvhfynfylpsihvjoc.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcGh2aGZ5bmZ5bHBzaWh2am9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDM0ODUsImV4cCI6MjA1ODA3OTQ4NX0.XBsyzYoZawBnygk7xgDjc0gjPnNZiLwSjyol8l_A4iU"
-);
-
-export { supabase as s };
