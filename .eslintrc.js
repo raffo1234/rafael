9a894ec0fa3fcd1cb0eaee7e608d555f248f84e8
Rafael <rhmcord@gmail.com>
2025-03-26 17:57:33 -0500

    feat: add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier
+  ],
+  rules: {
+    "@typescript-eslint/no-empty-function": "off",
+  },
+};
