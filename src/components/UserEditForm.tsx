57c8d941b2d06966e8b1142853bc50578f83475e
Rafael <rhmcord@gmail.com>
2025-03-26 10:51:42 -0500

    feat: remove unusud snippet code

--- src/components/UserEditForm.tsx
+++ src/components/UserEditForm.tsx
@@ -83,35 +83,6 @@ export default function UserEditForm({ user }: { user: User }) {
           Edit User
         </button>
       </footer>
-      {/* <!-- {
-            success === false && (
-              <div
-                role="alert"
-                className="mb-4 mt-10 relative flex w-full p-3 text-sm text-white bg-red-600 rounded-md"
-              >
-                {error}
-                <button
-                  className="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5"
-                  type="button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="h-5 w-5"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </div>
-            )
-          } --> */}
     </form>
   );
 }
